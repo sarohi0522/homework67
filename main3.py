3484b4baf26d1af88272a6e1a795fa69beee2055
sarohi0522 <sarohi@icloud.com>
2026-08-02 19:47:53 -0700

    Add files via upload

---
+++ main3.py
@@ -0,0 +1,8 @@
+import calendar
+
+# Loop through month numbers from 1 to 12
+for month_index in range(1, 13):
+    # Get the full name of the month
+    month_name = calendar.month_name[month_index]
+    # Display the month name
+    print(month_name)
